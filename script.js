08cee9067f257704dabb925a8b5aa10749f3e8f7
06nvibin <markangeloserrano11@gmail.com>
2025-11-03 00:16:24 +0800

    Remove shake effect from box
    Removed shaking effect from the box.

--- script.js
+++ script.js
@@ -3,15 +3,6 @@ const music = document.getElementById('bg-music');
 
 let opened = false;
 
-// Make it shake occasionally
-setInterval(() => {
-  if (!opened) {
-    box.classList.add('shake');
-    setTimeout(() => box.classList.remove('shake'), 500);
-  }
-}, 3000);
-
-// Open the box on click
 box.addEventListener('click', () => {
   if (!opened) {
     box.classList.add('open');
